317fb0872afd0db795e5861398337843c02543e1
biraj <birajbh65@gmail.com>
2026-08-25 22:59:47 +0545

    update config and add icons instead of texts in links

--- src/components/social_l.js
+++ src/components/social_l.js
@@ -1,17 +1,33 @@
+import Image from "next/image";
 export default function SocialLinks(){
     return(
         <div className="mt-8 flex gap-5">
         <a href="https://www.linkedin.com/in/thebirajh3ak3r/" target="_blank"
 rel="noopener noreferrer">
-            Linkedin
+    <Image 
+        src="/LinkedIn_icon.png" 
+        alt="Linkedin" 
+        width={30} 
+        height={20} 
+      />
         </a>
         <a href="https://www.github.com/birbh" target="_blank"
 rel="noopener noreferrer">
-            Github
+            <Image 
+        src="github_logo.png" 
+        alt="Linkedin" 
+        width={32} 
+        height={40} 
+      />
         </a>
         <a href="https://www.instagram.com/bir.__bht/" target="_blank"
 rel="noopener noreferrer">
-            Instagram
+            <Image 
+        src="/insta_logo.jpg" 
+        alt="Linkedin" 
+        width={60} 
+        height={20} 
+      />
         </a>
         </div>
     );
